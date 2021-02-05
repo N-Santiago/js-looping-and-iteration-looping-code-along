88484b48f2678b42e6ed56d816d1fde1ed885272
N-Santiago <norbertosantiago82@gmail.com>
2021-02-05 15:40:03 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
+
+function writeCards(cards) 
+{   
+  const message = [];
+    for (let i = 0; i < cards.length; i++) {
+      console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+      message.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+
+    return message;
+   
+  }
+
+  writeCards(cards);
+
+
+// function countDown(num)
+
+// let count = num 
+// while (count >= 0) {
+//   console.log(count--);
+// } 
+
+function countDown(num) {
+  let count = num
+  while (count >= 0) {
+    console.log(count--);
+  }
+}
